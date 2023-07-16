cf54e86de92960e0db0750fe40ba0e313f485f65
MohamedMokhtar6 <mohamed01017715716@gmail.com>
2023-07-16 19:25:22 +0300

    a

--- src/Components/util/CartItem.js
+++ src/Components/util/CartItem.js
@@ -4,48 +4,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { getOneProduct } from "../../redux/Actions/ProductAction";
 
 function CartItem({ item }) {
+  const dispatch = useDispatch();
+
   const [quantity, setQuantity] = useState(0);
+  const [change, setChange] = useState([]);
+
   const changeQuantity = (e) => {
     setQuantity(e.target.value);
   };
-  const [pro, setPro] = useState([]);
-  useEffect(() => {
-    // if (item.product) console.log(item);
-  }, [item]);
-  // if (item.product) {
-  //   pro = {
-  //     ...item.product,
-  //     poster: item.product.poster
-  //       ? "data:image/png;base64," + item.product.poster
-  //       : "",
-  //     brand: item.product.brand || {}, // make sure brand is defined
-  //     rate: item.product.rate, // make sure brand is defined
-  //     category: item.product.category || {},
-  //   };
-  // }
+  const handelDelete = (e) => {
+    console.log(e.target.parentElement.id);
+  };
 
   return (
     <Col className="my-2 d-flex px-2 justify-content-between">
-      {/* <img width="160px" height="197px" src={pro.poster} alt="" /> */}
+      <img
+        width="160px"
+        height="197px"
+        src={"data:image/png;base64," + item.product.poster}
+        alt=""
+      />
       <div className="">
         <Row className="justify-content-between">
-          <Col sm="12" className=" d-flex flex-row justify-content-between">
-            {/* <div className="d-inline pt-2 ">{pro.product.category.name}</div> */}
-            <div className="d-flex pt-2 click">
+          <Col sm="12" className=" d-flex flex-row justify-content-end">
+            <div
+              id={item.id}
+              className="d-flex pt-2 click"
+              onClick={handelDelete}
+            >
               <i className="fa-solid fa-trash text-danger"></i>
             </div>
           </Col>
         </Row>
         <Row className="justify-content-center mt-2">
           <Col sm="12" className=" d-flex flex-row justify-content-start">
-            {/* <div className="d-inline pt-2 ">{pro.name}</div> */}
+            <div className="d-inline pt-2 ">{item.product.name}</div>
           </Col>
         </Row>
-        <Row>
-          <Col sm="12" className="mt-1">
-            {/* <div className=" d-inline">brand : {pro.brand.name}</div> */}
-          </Col>
-        </Row>
+
         <Row>
           <Col sm="12" className="mt-1 d-flex">
             <div
@@ -70,8 +66,8 @@ function CartItem({ item }) {
                 <option value="3">3</option>
               </select>
             </div>
-            <div className="d-inline pt-2">{item.unitPrice} EGP</div>
           </Col>
+          <div className="d-inline pt-2">{item.unitPrice} EGP</div>
         </Row>
       </div>
     </Col>
--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -6,41 +6,27 @@ import CartCheckout from "../Components/util/CartCheckout ";
 import { useDispatch, useSelector } from "react-redux";
 import { getCart } from "../redux/Actions/CartAction";
 import { getOneProduct } from "../redux/Actions/ProductAction";
+import { getCartItem } from "../redux/Actions/CartItemsAction";
 
 function CartPage() {
   const dispatch = useDispatch();
   const cartId = localStorage.getItem("cartId");
   const [items, setItems] = useState([]);
-  const cart = useSelector((state) => state.cartReducer.cart);
+  const cart = useSelector((state) => state.cartItemsReducer.cartItem);
   const oneProduct = useSelector((state) => state.allproducts.oneProduct);
   const user = JSON.parse(localStorage.getItem("user"));
   const userId = user.userId;
 
-  const fetchCartItemDetails = async () => {
-    const updatedItems = [];
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-      await dispatch(getOneProduct(item.productId));
-      updatedItems.push({ ...item, product: oneProduct });
-    }
-    setItems(updatedItems);
-  };
-
   useEffect(() => {
-    dispatch(getCart(userId));
+    dispatch(getCartItem(cartId));
   }, [dispatch, userId]);
 
   useEffect(() => {
-    if (cart.items) {
-      setItems(cart.items);
+    if (cart) {
+      setItems(cart);
     }
   }, [cart]);
-
-  useEffect(() => {
-    if (items.length > 0) {
-      fetchCartItemDetails();
-    }
-  }, [items]);
+  // console.log(cart);
 
   return (
     <>
