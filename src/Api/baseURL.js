cf807d1698701f2b2111b3cefaa66dc45f53a0f5
MohamedMokhtar6 <mohamed01017715716@gmail.com>
2023-07-20 23:33:40 +0300

    a

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-  // baseURL: "http://online-store.somee.com",
-  baseURL: "https://localhost:7152",
+  baseURL: "http://online-store.somee.com",
+  // baseURL: "https://localhost:7152",
 });
 
 export default baseUrl;
